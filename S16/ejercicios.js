ae535afa53876f2821846ccaf1ae86ad3fbd98e8
DLinoC <danfer200017@gmail.com>
2022-11-17 18:00:57 -0500

    Tercer commit

---
+++ S16/ejercicios.js
@@ -0,0 +1,46 @@
+// Ejercicio 1:
+
+// let multipli=function(x,y){
+//     return x*y
+// }
+// let expon=function(x,y){
+//     return Math.pow(x,y)
+// }
+// let suma=(x,y) => {return x+y}
+
+// let resta=function(x,y){
+//     return x-y
+// }
+
+// var result =multipli(5,2)
+// alert(result)
+
+// result=expon(2,5)
+// alert(result)
+
+// result=suma(3,4)
+// alert(result)
+
+// result=resta(8,3)
+// alert(result)
+
+// Ejercicio 2:
+
+// let pasaraFarenheit = (grado) => {return Farenheit=grado*1.8 +32}
+
+// var result = pasaraFarenheit(150)
+// alert(result)
+
+// let pasaraCelsius = (grado) =>{return Celsius=(grado-32)/1.8}
+// var result =pasaraCelsius(result)
+// alert(result.toFixed(1))    
+
+// Ejercicio 3:
+
+// let num1=+prompt('Ingrese el primer numero')
+// let num2=+prompt('Ingrese el segundo numero')
+
+// let sumar = (x,y) => {
+//     return x+y 
+// }
+//  alert(sumar(num1,num2))
