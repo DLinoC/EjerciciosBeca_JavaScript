50cb31ad2e5d2a5c7abbd539b314b96a7c327fd6
DLinoC <danfer200017@gmail.com>
2022-11-24 14:44:53 -0500

    Se agregó la segunda sesión Js avanzado

--- S1-JsAvanced/ejercicios.js
+++ S1-JsAvanced/ejercicios.js
@@ -82,3 +82,49 @@ let pokemons = [
 //     agregarPokemon();
 //     console.log(MaestroPokemon.pokemon.sort( (a, b) => (b.base_damage + b.max_damage) - (a.base_damage + a.max_damage) ))
 // }
+/*//9. Crear una lista desordenada de Pokemons en nuestro documento HTML
+const root = document.getElementById("root")
+const ol = document.createElement('ol')
+root.append(ol)
+pokemons.forEach(pokemon => {
+    const li = document.createElement('li')
+    li.textContent = pokemon.name
+    li.addEventListener("click", () => setDamage(1))
+    ol.append(li)
+})*/
+
+//10. Utilizando javascript crear una tabla de pokemons con las siguientes columnas: id, name, type, base_damage, base_hp, speed
+ 
+const root = document.getElementById("root")
+const table = document.createElement("table")
+root.append(table)
+
+const tr=document.createElement("tr")
+table.append(tr)
+
+//Headers
+
+let headers = Object.keys(pokemons[0])
+
+for(index in headers){
+    const th=document.createElement("th")
+    th.textContent = headers[index]
+    tr.append(th)
+}
+
+//Table body
+for(let i=0;i<pokemons.length;i++){
+    const trb = document.createElement("tr")
+    const values =Object.values(pokemons[i])
+
+    for(let j=0; j<values.length; j++){
+        const tdb= document.createElement("td")
+        tdb.textContent= values[j]
+        trb.append(tdb)
+    }
+    table.append(trb)
+}
+
+//11. Utilizando javascript modifica el codigo creado en el ejecicio anterior y agrega un evento que permita ordenar los pokemons haciendo click en sus encabezados.
+ 
+//12. Corrige la function sortPokemons para que acepte ordenarlos segun id y name.
