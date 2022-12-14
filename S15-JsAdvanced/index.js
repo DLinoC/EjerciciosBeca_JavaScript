c1e7ca63c6430799018f82e0caffd63b189479a7
DLinoC <danfer200017@gmail.com>
2022-12-14 17:53:12 -0500

    se subio la sesion 15

---
+++ S15-JsAdvanced/index.js
@@ -0,0 +1,28 @@
+// const http=require('http');
+
+// const hostname='127.0.0.1';
+// const port=3000;
+
+// const server=http.createServer((req,res)=>{
+//     res.statusCode=200;
+//     res.setHeader('Content-type','text/plain');
+//     res.end('Danfer Aldair Lino Cardenas');
+// })
+
+// server.listen(port, hostname,() =>{
+//     console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`)
+// })
+
+// const express = require('express')
+
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//     res.send("2 + 3 = " + (2+3))
+// })
+
+// app.listen(port, () => {
+//     console.log(`el servido esta escuchando en http://localhost:${port}`) 
+//     console.log("2 + 3 = " + (2+3)) 
+// })
